1884da556de748179f3052d2cd3270eb60f63160
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-05-26 09:05:36 +0100

    Add setup file to allow package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='menu_pyg',
+      version='0.1',
+      description='Package to provide a menu sysem for Pygame (Python 2.7)',
+      url='http://github.com/thebillington/menu_pyg',
+      author='thebillington',
+      author_email='billy.rebecchi@googlemail.com',
+      license='MIT',
+      packages=['menu_pyg'],
+      zip_safe=False)
